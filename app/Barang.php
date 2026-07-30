ed25a2f5913ace1055f9ee2aca68e61aead7e1b8
zahrakeisha <zakeirah2008@gmail.com>
2026-07-30 14:23:13 +0700

    model beberapa

--- app/Barang.php
+++ app/Barang.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    //
+    protected $table = 'barangs';
+    protected $primaryKey = 'barang_id';
+    protected $fillable = [
+        'kategori_id',
+        'kode_barang',
+        'nama_barang',
+        'merek',
+        'model',
+        'serial_number',
+        'jumlah',
+        'satuan',
+        'harga_perolehan',
+        'tanggal_perolehan',
+        'masa_ekonomis',
+        'kondisi',
+        'lokasi',
+        'keterangan',
+    ];
+    
 }
--- app/Kategori.php
+++ app/Kategori.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
-    //
+    protected $table = 'kategoris';
+    protected $primaryKey = 'kategori_id';
+    protected $fillable = ['nama_kategori'];
+
+    public function barangs()
+    {
+        return $this->hasMany(Barang::class, 'kategori_id', 'kategori_id');
+    }
 }
