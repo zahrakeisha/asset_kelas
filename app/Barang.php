54dc7ea36b79cdb24982108fa50d098f45b06c3d
zahrakeisha <zakeirah2008@gmail.com>
2026-08-06 11:13:25 +0700

    ubah model

--- app/Barang.php
+++ app/Barang.php
@@ -6,44 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    protected $table = 'barangs';
-    protected $primaryKey = 'barang_id';
-
-    protected $fillable = [
-        'kategori_id',
-        'ruangan_id',
-        'masa_ekonomis_id',
-        'kode_barang',
-        'nama_barang',
-        'merek',
-        'model',
-        'serial_number',
-        'jumlah',
-        'kondisi',
-        'tanggal_perolehan',
-        'keterangan',
-    ];
-
-
-    public function kategori()
-    {
-        return $this->belongsTo(Kategori::class, 'kategori_id', 'kategori_id');
-    }
-
-    public function ruangan()
-    {
-        return $this->belongsTo(Ruangan::class, 'ruangan_id', 'ruangan_id');
-    }
-
-
-    public function masaEkonomis()
-    {
-        return $this->belongsTo(MasaEkonomis::class, 'masa_ekonomis_id', 'masa_ekonomis_id');
-    }
-
-
-    public function pengajuanBarangs()
-    {
-        return $this->hasMany(PengajuanBarang::class, 'barang_id', 'barang_id');
-    }
+   
 }
--- app/Kategori.php
+++ app/Kategori.php
@@ -7,12 +7,5 @@ use App\Barang;
 
 class Kategori extends Model
 {
-    protected $table = 'kategoris';
-    protected $primaryKey = 'kategori_id';
-    protected $fillable = ['nama_kategori'];
-
-    public function barangs()
-    {
-        return $this->hasMany(Barang::class, 'kategori_id', 'kategori_id');
-    }
+ ///
 }
