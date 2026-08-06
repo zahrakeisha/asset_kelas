058538f28bcc42b9efddf40d638b427d53b60ad9
rizqa-hania <rizqa.hania39@smk.belajar.id>
2026-08-06 13:26:53 +0700

    update pengajuan barang beserta model

---
+++ app/pengajuan_barang.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class pengajuan_barang extends Model
+{
+    protected $table = 'pengajuan_barang';
+    protected $primaryKey = 'pengajuan_id';
+    protected $fillable = [
+        'barang_id',
+        'tanggal_pengajuan',
+        'jenis_pengajuan',
+        'alasan',
+        'status',
+        'catatan'
+    ];
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'barang_id', 'barang_id');
+    }
+}
---
+++ database/migrations/2026_08_06_044349_create_pengajuan_barangs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengajuanBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuan_barangs', function (Blueprint $table) {
+            $table->bigIncrements('pengajuan_id');
+            $table->unsignedBigInteger('barang_id');
+            $table->date('tanggal_pengajuan');
+            $table->enum('jenis_pengajuan', ['Perbaikan', 'Penggantian', 'Penambahan']);
+            $table->text('alasan');
+            $table->enum('status', ['Menunggu', 'Disetujui', 'Ditolak'])->default('Menunggu');
+            $table->text('catatan')->nullable();
+            $table->foreign('barang_id')->references('barangs')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuan_barangs');
+    }
+}
