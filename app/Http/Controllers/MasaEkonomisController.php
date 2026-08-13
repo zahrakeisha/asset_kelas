27046dfd9103d790f67d23ad1b9768f268fa80f6
nurhazijah <hazizahnur088@gmail.com>
2026-08-13 10:51:53 +0700

    add masa ekonomis controller and routes

---
+++ app/Http/Controllers/MasaEkonomisController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Masa_ekonomis;
+use App\Kategori;
+
+class MasaEkonomisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         $masaEkonomis = Masa_ekonomis::with('kategori')->get();
+
+         return view('masa_ekonomis.index', compact('masaEkonomis'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+        $kategori = Kategori::all();
+
+        return view('masa_ekonomis.create', compact('kategori'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kategori_id' => 'required|exists:kategoris,kategori_id',
+            'lama_ekonomis' => 'required|integer',
+            'satuan' => 'required',
+            'keterangan' => 'nullable|string',
+        ]);
+
+        Masa_ekonomis::create([
+            'kategori_id' => $request->kategori_id,
+            'lama_ekonomis' => $request->lama_ekonomis,
+            'satuan' => $request->satuan,
+            'keterangan' => $request->keterangan,
+        ]);
+
+        return redirect()
+            ->route('masa_ekonomis.index')
+            ->with('success', 'Data masa ekonomis berhasil ditambahkan.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $masaEkonomis = Masa_ekonomis::findOrFail($id);
+        $kategori = Kategori::all();
+
+        return view('masa_ekonomis.edit', compact('masaEkonomis', 'kategori'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'kategori_id' => 'required|exists:kategoris,kategori_id',
+            'lama_ekonomis' => 'required|integer',
+            'satuan' => 'required',
+            'keterangan' => 'nullable|string',
+        ]);
+
+        $masaEkonomis = Masa_ekonomis::findOrFail($id);
+
+        $masaEkonomis->update([
+            'kategori_id' => $request->kategori_id,
+            'lama_ekonomis' => $request->lama_ekonomis,
+            'satuan' => $request->satuan,
+            'keterangan' => $request->keterangan,
+        ]);
+        return redirect()
+            ->route('masa_ekonomis.index')
+            ->with('success', 'Data masa ekonomis berhasil diperbarui.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $masaEkonomis = Masa_ekonomis::findOrFail($id);
+
+        $masaEkonomis->delete();
+
+        return redirect()
+            ->route('masa_ekonomis.index')
+            ->with('success', 'Data masa ekonomis berhasil dihapus.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\KategoriController;
+use App\Http\Controllers\MasaEkonomisController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
 
@@ -26,3 +40,10 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
         Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
         Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
     });
+
+        Route::get('/masa_ekonomis', 'MasaEkonomisController@index')->name('masa_ekonomis.index');
+        Route::get('/masa_ekonomis/create', 'MasaEkonomisController@create')->name('masa_ekonomis.create');
+        Route::post('/masa_ekonomis', 'MasaEkonomisController@store')->name('masa_ekonomis.store');
+        Route::get('/masa_ekonomis/{id}/edit', 'MasaEkonomisController@edit')->name('masa_ekonomis.edit');
+        Route::put('/masa_ekonomis/{id}', 'MasaEkonomisController@update')->name('masa_ekonomis.update');
+        Route::delete('/masa_ekonomis/{id}', 'MasaEkonomisController@destroy')->name('masa_ekonomis.destroy');
