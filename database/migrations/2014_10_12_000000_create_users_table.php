d6a7c17e18b09335afb211171b62a1603f4fa463
nafiisahzalfa4 <zalfaa.nafiisah28@smk.belajar.id>
2026-08-13 08:15:52 +0700

    edit migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
-            $table->integeger('no_telpon');
+            $table->integer('no_telpon');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
